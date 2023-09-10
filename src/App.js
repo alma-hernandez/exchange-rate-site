0e26a60cde94d2f217e76476e9e01acbf8407726
Alma Duffey <1984ahern@gmail.com>
2023-09-09 22:54:30 -0500

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+
 
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact render={() => <h1>Hello World</h1>} />
+        <Route render={() => <h1>404 NotFound</h1>} />
+      </Switch>
+    </Router>
   );
 }
 
