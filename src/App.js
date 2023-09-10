0dae5b73c52025b961aa3d44169fb3fc4b5ad3a1
Alma Duffey <1984ahern@gmail.com>
2023-09-09 23:40:07 -0500

    with dropdown

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import Layout from './Layout'
+import Layout from './Layout';
+import Home from './Home';
 
 
 const App = () => {
   return (
-
-    <Router>
+    <Router basename="exchange-rate-site">
       <Layout>
       <Switch>
-        <Route path="/" exact render={() => <h1>Hello World</h1>} />
+        <Route path="/" exact render={() => <Home />} />
         <Route render={() => <h1>404 Not Found</h1>} />
       </Switch>
       </Layout>
     </Router>
-
   );
 }
 
