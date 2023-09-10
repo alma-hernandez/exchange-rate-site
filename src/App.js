cbc8a70deea18d9cac4f4cf3dc210d6c9398a13e
Alma Duffey <1984ahern@gmail.com>
2023-09-09 23:06:19 -0500

    with nav and footer

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Layout from './Layout'
 
 
 const App = () => {
   return (
+
     <Router>
+      <Layout>
       <Switch>
         <Route path="/" exact render={() => <h1>Hello World</h1>} />
-        <Route render={() => <h1>404 NotFound</h1>} />
+        <Route render={() => <h1>404 Not Found</h1>} />
       </Switch>
+      </Layout>
     </Router>
+
   );
 }
 
